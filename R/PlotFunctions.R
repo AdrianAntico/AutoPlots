3ea307cd8a487faf0a9f575bda57fc102034e4c6
Adrian Antico <adrianantico@gmail.com>
2023-10-04 20:47:53 -0700

    Update PlotFunctions.R

--- R/PlotFunctions.R
+++ R/PlotFunctions.R
@@ -11901,7 +11901,7 @@ Plot.ROC <- function(dt = NULL,
     x1 = x[y == 1]; n1 = length(x1);
     x2 = x[y == 0]; n2 = length(x2);
     r = rank(c(x1,x2))
-    auc = (sum(r[1L:n1]) - n1 * (n1 + 1L) / 2) / n1 / n2
+    auc = (sum(r[1:n1]) - n1 * (n1 + 1) / 2) / n1 / n2
     return(auc)
   }
 
